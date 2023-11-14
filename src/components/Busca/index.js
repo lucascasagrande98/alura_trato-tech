a92b3dd39c6c13dbdfd08be3f529ff23bb670ed5
Lucas <lucascasagrande98@gmail.com>
2023-11-14 12:22:10 -0300

    projeto concluido

---
+++ src/components/Busca/index.js
@@ -0,0 +1,20 @@
+import styles from "./Busca.module.scss";
+import { mudarBusca, resetarBusca } from "store/reducers/busca";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function Busca() {
+  const busca = useSelector((state) => state.busca);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  useEffect(() => {
+    dispatch(resetarBusca());
+  }, [location.pathname, dispatch]);
+
+  return (
+    <div className={styles.busca}>
+      <input className={styles.input} placeholder="O que você procura?" value={busca} onChange={(evento) => dispatch(mudarBusca(evento.target.value))} />
+    </div>
+  );
+}
